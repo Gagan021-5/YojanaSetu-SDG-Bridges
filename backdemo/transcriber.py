594eb8b3a659e5e860acd04d98a699bdc0e5e55d
Gagan021-5 <gagansingh010221@gmail.com>
2025-06-24 13:27:18 +0530

    Fix: switched Whisper model to tiny to avoid Render memory crash

--- backdemo/transcriber.py
+++ backdemo/transcriber.py
@@ -5,8 +5,8 @@ sys.stdout.reconfigure(encoding='utf-8')  # enable emoji
 
 file_path = sys.argv[1]
 print("📂 Incoming file:", file_path)
+model = whisper.load_model("tiny")     #render ttake 512 mb but app is 1+ gb
 
-model = whisper.load_model("base")
 result = model.transcribe(file_path)
 
 print(f"{result['language']}|||{result['text']}")
